a47a0d73e19380ab733e28824c36afbd487b9398
jkClyde <aquin.randall@gmail.com>
2024-12-02 13:21:47 +0800

    deplyment fix 2

--- app/audio/page.jsx
+++ app/audio/page.jsx
@@ -1,10 +1,15 @@
-'use client'
+'use client';
 
-import AudioRecorder from "../components/AudioRecorder";
-export default function Home() {
+import dynamic from 'next/dynamic';
+
+const AudioRecorder = dynamic(() => import('../components/AudioRecorder'), {
+  ssr: false
+});
+
+export default function AudioPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className=" w-[95%] md:w-full max-w-md bg-white shadow-md rounded-lg p-6 ">
+      <div className="w-[95%] md:w-full max-w-md bg-white shadow-md rounded-lg p-6">
         <h1 className="text-2xl font-bold mb-4 text-center text-black">Audio Recorder</h1>
         <AudioRecorder />
       </div>
